eb80786d7398155bb120ab1f00a2e36a6ca5ace3
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-06-05 00:44:04 +0530

    Added CountryCard

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="root">
     <Nav />
-    <br><br><br>
-    <Filter />
+
+    <div class="container-fluid px-4">
+      <div class="row">
+        <div v-for="i in [1,2,3,4]" class="col-12 col-md-6 col-lg-3">
+          <CountryCard />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ components/country/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card mx-4 mb-5">
+    <img src="https://cdn.britannica.com/97/1597-004-05816F4E/Flag-India.jpg" alt="India" />
+
+    <div class="country-content ms-3 ms-lg-5 my-4">
+      <h3>India</h3>
+
+      <p class="mt-4 mb-1"><strong>Population:</strong> 1.5 Billion</p>
+      <p class="mb-1"><strong>Region:</strong> Asia</p>
+      <p><strong>Capital:</strong> New Delhi</p>
+    </div>
+
+  </div>
+</template>
