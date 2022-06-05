d7c16a8f5ae75417cdedf0c1539546ec5fca7896
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-06-06 00:20:59 +0530

    fixed home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="container-fluid px-4">
-    <div class="row">
+  <div class="container-fluid px-4 mt-5">
+    <div class="bars d-flex justify-content-between px-4">
+      <Search />
+      <Filter />
+    </div>
+    <div class="row mt-4">
       <div v-for="i in [1, 2, 3, 4]" class="col-12 col-md-6 col-lg-3">
         <CountryCard />
       </div>
