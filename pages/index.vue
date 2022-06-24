23eb676d2709ec1460f3ca94f73ca7e59e369316
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-06-24 11:50:39 +0530

    Fixed mobile spacing

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,28 @@ useHead({
 
 <template>
   <div class="container-fluid px-4 mt-5">
-    <div class="bars d-flex justify-content-between px-4">
+    <div class="sub-head px-4">
       <Search />
       <Filter />
     </div>
-    <div class="row mt-4">
+    <div class="row mt-5">
       <div v-for="i in [1, 2, 3, 4]" class="col-12 col-md-6 col-lg-3">
         <CountryCard />
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.sub-head {
+  display: flex;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .sub-head {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+}
+</style>
