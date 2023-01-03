d225403df244878ce1659f5682d81ad798ea0efd
Nishant Aanajney Jalan <cybercoder.nishant@gmail.com>
2023-01-03 12:44:37 +0530

    Added button to redirect to source code

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,10 @@
 
 <template>
   <div id="root" :class="isDarkTheme ? 'dark-theme' : null">
+    <a href="https://github.com/cybercoder-naj/countries.git" class="source-icon" target="_blank">
+      <i class="fa-solid fa-code"></i>
+    </a>
+
     <Nav />
     <slot />
   </div>
@@ -18,5 +22,24 @@ div#root {
 
 div#root.dark-theme {
   background: hsl(207, 26%, 17%)
+}
+
+.source-icon {
+  all: unset;
+  position: fixed;
+  bottom: 3em;
+  right: 3em;
+  z-index: 100;
+  background-color: lightblue;
+  aspect-ratio: 1;
+  display: grid;
+  place-items: center;
+  padding: 0.5em;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.source-icon i {
+  font-size: 3em;
 }
 </style>
